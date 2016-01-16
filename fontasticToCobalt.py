557ec990d2be78caa4715b134d19dd2002d8d322
shgz <roxane.paurobally.kristal@gmail.com>
2016-01-16 23:34:57 +0100

    Added command line argument for source and architechture Fixed a bug of undeclared variable when bad input given The script raise an error properly when file not found Some other code refractoring and code optimisation

--- fontasticToCobalt.py
+++ fontasticToCobalt.py
@@ -9,6 +9,7 @@ import os, errno
 import shutil
 import sys
 import imp
+import getopt
 
 class bcolors:
         OKBLUE = '\033[94m'
@@ -22,11 +23,12 @@ class bcolors:
 fontname = 'awesome'           # Change this before launching the script
 fontname = fontname.title()
 packagename = 'org.cobaltians'
+version = '0.3'
 debug = True
 
-# todo: create only the wanted package (optional parameter)
-android = True
-ios = True
+# Which parser to use (use optional parameter --sources=fontastic|icomoon)
+fontastic = False
+icomoon = False
 
 try:
     androidtpl = imp.load_source('templates_android', 'templates/tpl.android.py')
@@ -43,24 +45,83 @@ except:
     raise
 
 
-if (len(sys.argv) != 4):
-    print bcolors.FAIL + 'Usage: python', sys.argv[0], ' icons-references.html Fontxxx.ttf fontastic|icomoon' + bcolors.ENDC
-    exit(2)
+def main():
+        names = []
+        glyphs = []
 
-# todo: use an optional parameter for source: -s=htmlsource or --sources=htmlsource (getopts)
-fontastic = False
-icomoon = False
-if sys.argv[3] == 'fontastic':
-        fontastic = True
-elif sys.argv[3] == 'icomoon':
-        icomoon = True
-else:
-        print bcolors.FAIL + "Unknown html-type " + sys.argv[3] + ". Try 'fontastic' or 'icomoon'. Exiting..." + bcolors.ENDC
-        exit(3)
+        # Use optional parameter to create only the wanted package (--arch ios|android)
+        android = True
+        ios = True
 
-# storage
-names = []
-glyphs = []
+        # Use source parameter to specify the HTML web-site source (--source fontastic|icomoon)
+        source = None
+
+        # Needed parameters
+        html_file_name = None
+        ttf_file_name = None
+
+        # Parse arguments
+        try:
+                options, remainder = getopt.getopt(sys.argv[1:], 's:a:n:h:v', ['source=', 'arch=', 'name=', 'help'])
+        except getopt.GetoptError as err:
+                print str(err) # print help information and exit
+                sys.exit(2)
+
+        for opt, arg in options:
+                if opt in ('-s', '--source'):
+                        if arg == 'fontastic' or arg == 'icomoon':
+                                source = arg
+                        else: print bcolors.FAIL + "I don't have any parser for " + arg + " yet. Try 'fontastic' or 'icomoon'" + bcolors.ENDC
+                elif opt in ('-a', '--arch'):
+                        if arg == 'android': ios = False
+                        elif arg == 'ios': android = False
+                        else:
+                                print bcolors.FAIL + arg + ": this architecture is not supported yet. Try 'android' or 'ios'" + bcolors.ENDC
+                                exit(1)
+                elif opt in ('-h', '--help'):
+                        print bcolors.BOLD + 'Usage: python fontRefToCobalt -s fontastic|icomoon -a android icons-references.html Fontxxx.ttf' + bcolors.ENDC
+                        sys.exit(0)
+                elif opt in ('-n', '--name'):
+                        logme('Set ' + arg + ' as font name.')
+                        fontname = arg.title()
+                elif opt in ('-v'):
+                        print 'Version', version
+                        exit(0)
+
+        html_file_name = remainder[0]
+        ttf_file_name = remainder[1]
+        
+        # Opening HTML file
+        logme('Opening ' + html_file_name + ' and parsing HTML...')
+        try:
+                file = open(html_file_name, 'r')
+        except IOError as exc:
+                logme(bcolors.FAIL + 'Error: File ' + html_file_name + ' not found' + bcolors.ENDC)
+                exit(4)
+
+        # IcoMoon or fontastic (there are more to come)
+        if source == 'icomoon': parser = icomoon_parser
+        elif source == 'fontastic': parser = fontastic_parser
+
+        # Starting to parse HTML
+        parser.parseHTML(file)
+        names = parser.get_names()
+        glyphs = parser.get_glyphs()
+
+        # End parsing
+        file.close()
+
+        # Create packages
+        if android == True:
+                android_package_creator(fontname, names, glyphs, ttf_file_name).create()
+                print bcolors.OKGREEN + ' * Android ' + fontname + ' font package added.' + bcolors.ENDC
+        if ios == True:
+                ios_package_creator(fontname, names, glyphs, ttf_file_name).create()
+                print bcolors.OKGREEN + ' * IOS ' + fontname + ' font package added.' + bcolors.ENDC
+
+#
+# Functions and class definitions
+#
 
 # Usage :
 # str   : what to log
@@ -84,136 +145,151 @@ def mkdir_p(path):
 # content : the content to add in the file (ex: "Copyright (c) 2015 Cobaltians...")
 def generatefile(name, path, content):
     logme(bcolors.OKBLUE + 'Generating ' + path + name + '...' + bcolors.ENDC)
-    file = open(path + name, "w")
+    try:
+            file = open(path + name, "w")
+    except IOError as exc:
+            print bcolors.FAIL + 'Error: File ' + path + name + ' not found' + bcolors.ENDC
+            raise
     file.write(content)
     file.close
 
+# Usage:
+# src  : file to copy
+# dst  : where to copy it
 def copyfile(src, dst):
     logme(bcolors.OKBLUE + 'Copying ' + src + ' to ' + dst + '...' + bcolors.ENDC)
     shutil.copy(src, dst)
 
-# Starting to parse HTML
-logme('Opening ' + sys.argv[1] + ' and parsing HTML...')
-file = open(sys.argv[1], 'r')
+#
+# Generate an Ios librairy containing the font
+# fontname : name of the package
+# names : icons names
+# glyphs : icons glyphs
+# ttf : true type file path
+#
+class ios_package_creator(object):
+        def __init__(self, fontname, names, glyphs, ttf):
+                self.fontname = fontname
+                self.names = names
+                self.glyphs = glyphs
+                self.ttf = ttf
 
-# IcoMoon parsing
-if icomoon == True:
-        icomoon_parser.parseHTML(file)
-        names = icomoon_parser.get_names()
-        glyphs = icomoon_parser.get_glyphs()
+        def create(self):
+                logme(bcolors.BOLD + 'Starting to create IOS ' + self.fontname + ' package.' + bcolors.ENDC)
+                prefix = 'f' + self.fontname[0]
+                upperprefix = prefix.upper()    # ex: home -> @FAhome
+                lowerprefix = prefix.lower()    # ex: fontAwesome -> fa
 
-# fontastic parsing
-elif fontastic == True:
-        fontastic_parser.parseHTML(file)
-        names = fontastic_parser.get_names()
-        glyphs = fontastic_parser.get_glyphs()
-       
-# End parsing
-file.close()
+                # identifiers list generation: glass -> fa-glass
+                identifiers = []
 
-# Android package architecture
-packagepath = packagename.replace(".", "/") + '/'
-fontpath = 'Fonts-Font' + fontname + '-Android/'
-assetspath = fontpath + 'src/main/assets/'
-javapath = fontpath + 'src/main/java/' + packagepath + 'fonts/font' + fontname + '/'
-drawablepath = fontpath + 'src/main/res/drawable/'
-valuepath = fontpath + 'src/main/res/values/'
+                for name in self.names:
+                        identifiers.append(lowerprefix + '-' +  name.replace("_", "-"))
 
-# IOS package architecture
-iospackagepath = 'Fonts-Font' + fontname + '-iOS/'
+                # Token dictionary generation: fa-glass, &#xxxxx -> @"fa-glass" : @"\uxxxx",\n
+                tokenlist = ''
+                nbglyph = len(self.glyphs)
+                it = 1
 
-# Android pakage
-logme(bcolors.BOLD + 'Starting to create Android ' + fontname + ' package.' + bcolors.ENDC)
+                for identifier, glyph in zip(identifiers, self.glyphs):
+                        tokenlist = tokenlist + '        @"' + identifier + '": ' + glyph + '"'
+                        if it != nbglyph:
+                                tokenlist = tokenlist + ',\n'
+                        it = it + 1
 
-# creating package architecture
-mkdir_p(assetspath)
-mkdir_p(javapath)
-mkdir_p(drawablepath)
-mkdir_p(valuepath)
+                tokenlist = tokenlist.replace('&#x', '@"\u') # converting to ios .m format
+                tokenlist = tokenlist.strip()
 
-# Create xml file
-logme('Setting strings.xml file infos...')
-doc = Document()
-base = doc.createElement('resources')
-doc.appendChild(base)
+                # IOS package architecture
+                iospackagepath = 'Fonts-Font' + self.fontname + '-iOS/'
 
-for name, glyph in zip(names, glyphs):
-    entry = doc.createElement('string')
-    base.appendChild(entry)
-    entry.setAttribute("name", name)
-    entry.setAttribute("translatable"  , "false")
-    entry_content = doc.createTextNode(glyph)
-    entry.appendChild(entry_content)
+                # building IOS architecture
+                mkdir_p(iospackagepath)
 
-xmltxt = doc.toprettyxml(indent="    ", encoding="utf-8")
-xmltxt = xmltxt.replace('&amp;', '&') # utf-8 fix
-generatefile('strings.xml', valuepath, xmltxt)
+                # Creating FontFontastic.h
+                generatefile('Font' + self.fontname + '.h', iospackagepath,
+                             iostpl.tplfonth.substitute(fontkey = self.fontname))
 
-# Create values.xml
-generatefile('values.xml', valuepath, androidtpl.values)
+                # Creating FontFontastic.m
+                generatefile('Font' + self.fontname + '.m', iospackagepath,
+                             iostpl.tplfontm.substitute(fontkey = self.fontname, tokenlistkey = tokenlist))
 
-# Create build.gradle
-generatefile('build.gradle', fontpath, androidtpl.gradle)
+                # Creating LICENCE file
+                generatefile('LICENCE', iospackagepath, iostpl.licence)
 
-# Create LICENCE file
-generatefile('LICENCE', fontpath, androidtpl.licence)
+                # Copy font.ttf
+                path = iospackagepath + 'Font' + self.fontname + '.ttf'
+                copyfile(self.ttf, path)
 
-# Create Manifest.xml
-generatefile('AndroidManifest.xml', fontpath + 'src/main/', androidtpl.manifest.substitute(packagenamekey = packagename, fontkey = fontname))
+#
+# Generate an android module containing the font
+# fontname : name of the package
+# names : icons names
+# glyphs : icons glyphs
+# ttf : true type file path
+#
+class android_package_creator(object):
+        def __init__(self, fontname, names, glyphs, ttf):
+                self.fontname = fontname
+                self.names = names
+                self.glyphs = glyphs
+                self.ttf = ttf
 
-# Create FontDrawable.java
-generatefile('Font' + fontname + 'Drawable.java', javapath, androidtpl.javadrawable.substitute(fontkey=fontname, packagekey=packagename))
+        def create(self):
+                logme(bcolors.BOLD + 'Starting to create Android ' + self.fontname + ' package.' + bcolors.ENDC)
+                # Android package architecture
+                packagepath = packagename.replace(".", "/") + '/'
+                fontpath = 'Fonts-Font' + self.fontname + '-Android/'
+                assetspath = fontpath + 'src/main/assets/'
+                javapath = fontpath + 'src/main/java/' + packagepath + 'fonts/font' + self.fontname + '/'
+                drawablepath = fontpath + 'src/main/res/drawable/'
+                valuepath = fontpath + 'src/main/res/values/'
 
-# Copy font.ttf
-path = assetspath + 'Font' + fontname + '.ttf'
-copyfile(sys.argv[2], path)
+                # creating package architecture
+                mkdir_p(assetspath)
+                mkdir_p(javapath)
+                mkdir_p(drawablepath)
+                mkdir_p(valuepath)
+                
+                # Create xml file
+                logme('Setting strings.xml file infos...')
+                doc = Document()
+                base = doc.createElement('resources')
+                doc.appendChild(base)
+                
+                for name, glyph in zip(self.names, self.glyphs):
+                        entry = doc.createElement('string')
+                        base.appendChild(entry)
+                        entry.setAttribute("name", name)
+                        entry.setAttribute("translatable"  , "false")
+                        entry_content = doc.createTextNode(glyph)
+                        entry.appendChild(entry_content)
+                        
+                xmltxt = doc.toprettyxml(indent="    ", encoding="utf-8")
+                xmltxt = xmltxt.replace('&amp;', '&') # utf-8 fix
+                generatefile('strings.xml', valuepath, xmltxt)
 
-####### IOS packaging
+                # Create values.xml
+                generatefile('values.xml', valuepath, androidtpl.values)
 
-if ios == True : logme(bcolors.BOLD + 'Starting to create IOS ' + fontname + ' package.' + bcolors.ENDC)
+                # Create build.gradle
+                generatefile('build.gradle', fontpath, androidtpl.gradle)
 
-prefix = 'f' + fontname[0]
-upperprefix = prefix.upper()    # ex: home -> @FAhome
-lowerprefix = prefix.lower()    # ex: fontAwesome -> fa
+                # Create LICENCE file
+                generatefile('LICENCE', fontpath, androidtpl.licence)
 
-# identifier list generation: glass -> fa-glass
-identifiers = []
+                # Create Manifest.xml
+                generatefile('AndroidManifest.xml', fontpath + 'src/main/',
+                             androidtpl.manifest.substitute(packagenamekey = packagename, fontkey = self.fontname))
 
-for name in names:
-    identifiers.append(lowerprefix + '-' +  name.replace("_", "-"))
+                # Create FontDrawable.java
+                generatefile('Font' + self.fontname + 'Drawable.java', javapath,
+                             androidtpl.javadrawable.substitute(fontkey=self.fontname, packagekey=packagename))
 
-# dictionary token string generation: fa-glass,FAglass -> tmp[@"fa-glass"]= @(FAglass);\n
-tokenlist = ''
-nbglyph = len(glyphs)
-it = 1
+                # Copy font.ttf
+                path = assetspath + 'Font' + self.fontname + '.ttf'
+                copyfile(self.ttf, path)
 
-for identifier, glyph in zip(identifiers, glyphs):
-    tokenlist = tokenlist + '        @"' + identifier + '": ' + glyph + '"'
-    if it != nbglyph:
-        tokenlist = tokenlist + ',\n'
-    it = it + 1
-tokenlist = tokenlist.replace('&#x', '@"\u'); # converting to ios .m format todo: replace + delete ';'
-tokenlist = tokenlist.strip()
-
-# building IOS architecture
-mkdir_p(iospackagepath)
-
-# Creating FontFontastic.h
-generatefile('Font' + fontname + '.h', iospackagepath, iostpl.tplfonth.substitute(fontkey = fontname))
-
-# Creating FontFontastic.m
-generatefile('Font' + fontname + '.m', iospackagepath, iostpl.tplfontm.substitute(fontkey = fontname, tokenlistkey = tokenlist))
-
-# Creating LICENCE file
-generatefile('LICENCE', iospackagepath, iostpl.licence)
-
-# Copy font.ttf
-path = iospackagepath + 'Font' + fontname + '.ttf'
-copyfile(sys.argv[2], path)
-
-print 'Jobs done:'
-if android == True:
-    print bcolors.OKGREEN + ' * Android ' + fontname + ' font package added.' + bcolors.ENDC
-if ios == True:
-    print bcolors.OKGREEN + ' * IOS ' + fontname + ' font package added.' + bcolors.ENDC
+if __name__ == "__main__":
+        main()
 exit(0)
--- parsers/fontastic.py
+++ parsers/fontastic.py
@@ -8,19 +8,23 @@ names = []
 glyphs = []
 
 class FontasticParser (HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.ulposition = 0
+        
     def handle_starttag(self, tag, attrs):
-        global ulmapping
+
         for name, value in attrs:
             if tag == 'ul':
                 if value == 'glyphs css-mapping':
-                    ulmapping = True;
+                    self.ulposition = 1;
                 elif value == 'glyphs character-mapping':
-                    ulmapping = False;
+                    self.ulposition = 2;
             if tag == 'input':
                 if name == 'value':
-                    if ulmapping == True:
+                    if self.ulposition == 1:
                         names.append(value.replace("-", "_"))
-                    elif ulmapping == False:
+                    elif self.ulposition == 2:
                         glyphs.append(value.replace("-", "_"))
     
 def parseHTML(file):
